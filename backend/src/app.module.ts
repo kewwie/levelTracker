97d265cc136ba7803046b9b170918195146a86b8
Kewi <iamkewwie@gmail.com>
2024-02-03 01:44:10 +0100

    Added back typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+
+// Everything TypeORM
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from './data/datasource';
 
 @Module({
     imports: [
+        TypeOrmModule.forRoot(dataSourceOptions),
         UserModule
     ],
     controllers: [AppController],
