30b607a541964f9e88d969935048ef3589657571
Kewi <iamkewwie@gmail.com>
2024-02-03 23:56:22 +0100

    Fixes for random stuff

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 
 // Everything TypeORM
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -10,7 +9,6 @@ import { dataSourceOptions } from './data/datasource';
 @Module({
     imports: [
         TypeOrmModule.forRoot(dataSourceOptions),
-        UserModule
     ],
     controllers: [AppController],
     providers: [AppService],
