4ed8af2a5ac2298749b0b2cc973a05b0f943a09e
Kewi <iamkewwie@gmail.com>
2024-02-03 21:43:04 +0100

    Updated files with spaces and unused files

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    getHello(): string {
+        return 'Hello World!';
+    }
 
-  getKew(): string {
-    return "This service was created by k3wwie";
-  }
+    getKew(): string {
+        return "This service was created by k3wwie";
+    }
 }
