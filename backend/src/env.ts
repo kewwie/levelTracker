7e1413fc3cdc28830c9fd02547a8facbedd2a489
Kewi <iamkewwie@gmail.com>
2024-02-03 03:23:57 +0100

    Working On Development Version

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -2,8 +2,18 @@ import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.join(__dirname, "../../.env") });
 
+let DB_HOST;
+if (process.env.NODE_ENV === "DEVELOPMENT") {
+    DB_HOST = "localhost";
+} else {
+    DB_HOST = "mysql";
+}
+
+console.log(DB_HOST);
+console.log(process.env.DATABASE_PASSWORD)
+
 export const env = {
-    DB_HOST: "localhost",
+    DB_HOST: DB_HOST,
     DB_PORT: parseInt(process.env.DATABASE_PORT),
     DB_USER: "levels",
     DB_PASSWORD: process.env.DATABASE_PASSWORD,
