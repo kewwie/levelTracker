ce5081b694e257b4b511b98ca4a233d00025c3f1
Kewi <iamkewwie@gmail.com>
2024-02-03 01:43:03 +0100

    Fixing env values

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 import path from "path";
-dotenv.config({ path: path.join(__dirname, "../../.env") })
+dotenv.config({ path: path.join(__dirname, "../../.env") });
 
 export const env = {
-    DB_HOST: "mysql",
+    DB_HOST: "localhost",
     DB_PORT: parseInt(process.env.DATABASE_PORT),
     DB_USER: "levels",
     DB_PASSWORD: process.env.DATABASE_PASSWORD,
