9adcdde723f32041aeb1ba1d167fc4c96a371794
Kewi <iamkewwie@gmail.com>
2024-02-03 03:31:43 +0100

    Made changes so we can copy .env to each container so they can access it

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import path from "path";
-dotenv.config({ path: path.join(__dirname, "../../.env") });
+dotenv.config({ path: path.join(__dirname, "../.env") });
 
 let DB_HOST;
 if (process.env.NODE_ENV === "DEVELOPMENT") {
