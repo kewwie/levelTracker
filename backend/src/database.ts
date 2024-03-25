7f5e6d30af93b59ebbcef4f1c74bcb21c199d39e
Kewi <iamkewwie@gmail.com>
2024-03-25 22:50:15 +0100

    Update database.ts

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -62,7 +62,7 @@ export const Download = async (guildId: number, type: string) => {
                     rank: member.rank,
                     level: member.level,
                     xp: member.xp.totalXp,
-                    averageXp: (existingMember.averageXp + xpGain) / msgGain,
+                    averageXp: (existingMember.averageXp + xpGain) / (msgGain || 0),
                     hourlyXp: existingMember.hourlyXp + xpGain,
                     dailyXp: existingMember.dailyXp + xpGain,
                     weeklyXp: existingMember.weeklyXp + xpGain,
