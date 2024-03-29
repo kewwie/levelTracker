67dc1b1ad9f403bb89c46766c334e13bfb7e3348
Kewi <iamkewwie@gmail.com>
2024-03-29 22:10:11 +0100

    Removed env from frontend

--- frontend/src/env.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.join(__dirname, "../.env") });
-
-export const env = {
-    URL: process.env.URL as string,
-    INTERNAL_URL: "http://nginx" as string,
-}
