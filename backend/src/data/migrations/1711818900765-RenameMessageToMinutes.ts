dcc9bdeb9515272aa6316db36e8994be63e5753e
Kewi <iamkewwie@gmail.com>
2024-03-30 19:55:38 +0100

    Fixed Migration

--- backend/src/data/migrations/1711818900765-RenameMessageToMinutes.ts
+++ backend/src/data/migrations/1711818900765-RenameMessageToMinutes.ts
@@ -3,19 +3,18 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class RenameMessageToMinutes1711818900765 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE members RENAME COLUMN messages TO minutes");
-        await queryRunner.query("ALTER TABLE members RENAME COLUMN monthlyMsg TO monthlyMinutes");
-        await queryRunner.query("ALTER TABLE members RENAME COLUMN weeklyMsg TO weeklyMinutes");
-        await queryRunner.query("ALTER TABLE members RENAME COLUMN dailyMsg TO dailyMinutes");
-        await queryRunner.query("ALTER TABLE members RENAME COLUMN hourlyMsg TO hourlyMinutes");
+        await queryRunner.renameColumn("members", "messages", "minutes")
+        await queryRunner.renameColumn("members", "monthlyMsg", "monthlyMinutes");
+        await queryRunner.renameColumn("members", "weeklyMsg", "weeklyMinutes");
+        await queryRunner.renameColumn("members", "dailyMsg", "dailyMinutes");
+        await queryRunner.renameColumn("members", "hourlyMsg", "hourlyMinutes");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE members RENAME COLUMN minutes TO messages");
-        await queryRunner.query("ALTER TABLE members RENAME COLUMN monthlyMinutes TO monthlyMsg");
-        await queryRunner.query("ALTER TABLE members RENAME COLUMN weeklyMinutes TO weeklyMsg");
-        await queryRunner.query("ALTER TABLE members RENAME COLUMN dailyMinutes TO dailyMsg");
-        await queryRunner.query("ALTER TABLE members RENAME COLUMN hourlyMinutes TO hourlyMsg");
+        await queryRunner.renameColumn("members", "hourlyMinutes", "hourlyMsg");
+        await queryRunner.renameColumn("members", "dailyMinutes", "dailyMsg");
+        await queryRunner.renameColumn("members", "weeklyMinutes", "weeklyMsg");
+        await queryRunner.renameColumn("members", "monthlyMinutes", "monthlyMsg");
+        await queryRunner.renameColumn("members", "minutes", "messages");
     }
-
 }
