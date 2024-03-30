df418c21a8f9f6c65476610864d37e0b5ec53cb7
Kewi <iamkewwie@gmail.com>
2024-03-30 02:27:40 +0100

    Added stats command

---
+++ bot/src/commands/stats.ts
@@ -0,0 +1,46 @@
+import { Command } from "../types/Command";
+import { env } from "../env";
+import { MemberResponse } from "../types/MemberResponse";
+import { EmbedBuilder } from "discord.js";
+import { StyleNumber } from "../utils/styleNumber";
+
+export const command: Command = {
+    config: {
+        name: "stats",
+        description: "Get a users daily level stats.",
+        type: 1,
+        contexts: [0],
+        integration_types: [0, 1],
+        options: [
+            {
+                name: "member",
+                description: "The member you want to get information about.",
+                type: 6,
+                required: false,
+            }
+        ]
+    },
+    async execute(client, interaction) {
+        let data: MemberResponse  = await fetch(
+            env.INTERNAL_API + "/guilds/" + interaction.guildId + "/users/" + (interaction.options.get("member")?.value || interaction.user.id), 
+        ).then(res => res.json());
+
+        if (data.message) {
+            return interaction.reply(data.message);
+        }
+        
+        var embed = new EmbedBuilder()
+            .setColor("#0099ff")
+            .setAuthor({ name: data.tag, iconURL: data.avatarUrl })
+            .addFields(
+                { name: "Hourly", value: `${StyleNumber(data.hourlyMsg)} Messages\n${StyleNumber(data.hourlyXp)} XP`, inline: true },
+                { name: "Daily", value: `${StyleNumber(data.dailyMsg)} Messages\n${StyleNumber(data.dailyXp)} XP`, inline: true },
+                { name: "Weekly", value: `${StyleNumber(data.weeklyMsg)} Messages\n${StyleNumber(data.weeklyXp)} XP`, inline: true },
+                { name: "Monthly", value: `${StyleNumber(data.monthlyMsg)} Messages\n${StyleNumber(data.monthlyXp)} XP`, inline: true },
+            )
+            .setTimestamp()
+            .setFooter({ text: `You can view a leaderboard about these stats.` })
+    
+        await interaction.reply({ embeds: [embed], ephemeral: false });
+    }
+}
