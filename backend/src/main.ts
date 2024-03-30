2bf845cf3bca8ff9c5ff66e170441c290cee1d54
Kewi <iamkewwie@gmail.com>
2024-03-30 22:29:00 +0100

    Added timezones to jobs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { dataSource } from './data/datasource';
-import { scheduleJob } from "node-schedule";
+import { scheduleJob, RecurrenceRule } from "node-schedule";
 import { Guild } from './data/entities/Guild';
 import { Download, ResetLeaderboard } from './database';
 
@@ -10,33 +10,58 @@ async function bootstrap() {
     await dataSource.initialize();
     await app.listen(2000);
 
-    scheduleJob('0 * * * *', () => { 
+    var hourly = new RecurrenceRule();
+    hourly.tz = 'CST';
+    hourly.minute = 0;
+
+    scheduleJob(hourly, () => { 
         console.log(`[DATABASE] Reset Hourly Leaderboards`);
         ResetLeaderboard("hourly");
     });
 
-    scheduleJob('0 0 * * *', () => { 
+    var daily = new RecurrenceRule();
+    daily.tz = 'CST';
+    daily.minute = 0;
+    daily.hour = 0;
+
+    scheduleJob(daily, () => { 
         console.log(`[DATABASE] Reset Daily Leaderboards`);
         ResetLeaderboard("daily");
     });
 
-    scheduleJob('0 0 * * 1', () => { 
+    var weekly = new RecurrenceRule();
+    weekly.tz = 'CST';
+    weekly.minute = 0;
+    weekly.hour = 0;
+    weekly.dayOfWeek = 1;
+
+    scheduleJob(weekly, () => { 
         console.log(`[DATABASE] Reset Weekly Leaderboards`);
         ResetLeaderboard("weekly");
     });
 
-    scheduleJob('0 0 1 * *', () => { 
+    var monthly = new RecurrenceRule();
+    monthly.tz = 'CST';
+    monthly.minute = 0;
+    monthly.hour = 0;
+    monthly.date = 1;
+
+    scheduleJob(monthly, () => { 
         console.log(`[DATABASE] Reset Monthly Leaderboards`);
         ResetLeaderboard("monthly");
     });
 
-    setInterval(async () => {
+    var loop = new RecurrenceRule();
+    loop.tz = 'CST';
+    loop.second = 0;
+
+    scheduleJob(loop, async () => {
         let guilds = await dataSource.getRepository(Guild).find();
 
         for (let guild of guilds) {
             console.log(`[DATABASE] [LOOP] [${guild.id}] ${guild.name}`);
             Download(guild.id, guild.type);
         }
-    }, 60 * 1000) // 1 Minute
+    }); // 1 Minute
 }
 bootstrap();
