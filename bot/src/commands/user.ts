e677644f281504c2d000ac21821884a1109ad087
Kewi <iamkewwie@gmail.com>
2024-03-30 22:22:30 +0100

    Added minutes and replaced messages

--- bot/src/commands/user.ts
+++ bot/src/commands/user.ts
@@ -41,7 +41,7 @@ export const command: Command = {
                 { name: "Level", value: `${data.level}`, inline: true },
                 { name: "Average XP", value: `${StyleNumber(data.averageXp || 20)}`, inline: true },
 
-                { name: "Total", value: `${StyleNumber(data.messages)} Messages \n${StyleNumber(data.xp)} Xp`, inline: true },
+                { name: "Total", value: `${StyleNumber(data.minutes)} Minutes \n${StyleNumber(data.xp)} Xp`, inline: true },
             )
             .setTimestamp()
             .setFooter({ text: `You need to chat${StyleNumber((xp - data.xp) / (data.averageXp || 20))} minutes to level up` })
