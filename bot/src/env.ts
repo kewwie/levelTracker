6850b68870e60e6f79de52d87dca15c970506b50
Kewi <iamkewwie@gmail.com>
2024-03-30 01:06:01 +0100

    Fixing stuff

--- bot/src/env.ts
+++ bot/src/env.ts
@@ -2,10 +2,9 @@ import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
-
 var INTERNAL_URL
 
-if (process.env.PORT === "433") {
+if (process.env.PORT === "443") {
     INTERNAL_URL = "https://nginx:443";
 } else {
     INTERNAL_URL = "http://nginx";
@@ -17,5 +16,5 @@ export const env = {
     CLIENT_TOKEN: process.env.CLIENT_TOKEN as string,
 
     URL: process.env.URL as string,
-    INTERNAL_URL: INTERNAL_URL as string,
+    INTERNAL_URL: "http://backend:2000" as string,
 }
